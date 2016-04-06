336ee7096be39024a6b409bb00b40d59c72b67ac
npnataly <npnataly@gmail.com>
2016-04-06 20:32:08 +0300

    Comment the line again

--- src/test/java/TestingAbv.java
+++ src/test/java/TestingAbv.java
@@ -20,7 +20,7 @@ public class TestingAbv {
         //Creating our WebDriver and setting up the needed timeout
 
         //driver = new FirefoxDriver();
-        driver.manage().timeouts().implicitlyWait(6, TimeUnit.SECONDS);
+        //driver.manage().timeouts().implicitlyWait(6, TimeUnit.SECONDS);
     }
     
 
